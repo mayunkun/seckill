cb57601570685eb59a2b8d2bab2d2844625da986
mayunkun <1163363720@qq.com>
2020-11-04 15:54:05 +0800

    Init

---
+++ src/test/java/com/aeert/seckill/SeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeert.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
